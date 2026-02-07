1791f03f195a313ade875fae420d3a356b209b8f
NimaiDev <nimaic.dev@gmail.com>
2026-02-07 20:05:45 +0530

    Trying layouts

--- index.ts
+++ index.ts
@@ -1,15 +1,30 @@
-import { createCliRenderer, Text } from "@opentui/core";
+import { BoxRenderable, createCliRenderer } from "@opentui/core"
 
-const renderer = await createCliRenderer({
-    exitOnCtrlC: true
+const renderer = await createCliRenderer()
+
+const container = new BoxRenderable(renderer, {
+  id: "container",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  height: 10,
 })
 
+const leftPanel = new BoxRenderable(renderer, {
+  id: "left",
+  flexGrow: 1,
+  height: 10,
+  backgroundColor: "#444",
+})
 
-renderer.root.add(
-    Text(
-        {
-            content: "Hello World!",
-            fg: "#FF0000"
-        }
-    )
-)
+const rightPanel = new BoxRenderable(renderer, {
+  id: "right",
+  width: 20,
+  height: 10,
+  backgroundColor: "#666",
+})
+
+container.add(leftPanel)
+container.add(rightPanel)
+renderer.root.add(container)
