8673dffa2cf1a2a8032c74ee50f4f0b15f530ea9
NimaiDev <nimaic.dev@gmail.com>
2026-02-10 20:50:03 +0530

    Working with inputs

--- src/types/react-shim.d.ts
+++ src/types/react-shim.d.ts
@@ -2,6 +2,8 @@ declare module "react" {
   export type SetStateAction<S> = S | ((prevState: S) => S)
   export type Dispatch<A> = (value: A) => void
   export type ReactNode = unknown
+  export type DependencyList = readonly unknown[]
+  export type EffectCallback = () => void | (() => void)
   export function useState<S>(
     initialState: S | (() => S),
   ): [S, Dispatch<SetStateAction<S>>]
